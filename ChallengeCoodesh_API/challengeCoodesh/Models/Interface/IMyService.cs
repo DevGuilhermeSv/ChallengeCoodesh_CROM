1e9d2d2e6b18295da5b29da668484f05e811b022
Guilherme Rodrigues <gui_s@DESKTOP-H4ERN8E>
2021-12-15 16:25:54 -0300

    Added API Project

---
+++ ChallengeCoodesh_API/challengeCoodesh/Models/Interface/IMyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeCoodesh.Models.Interface
+{
+    public interface IMyService
+    {
+        Task<HttpResponseMessage> GetPage();
+    }
+}
